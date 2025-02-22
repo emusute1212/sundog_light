1b49bc47c79c7f8996ae8138537506c50c9ed50b
emusute1212 <miyanishi1212@gmail.com>
2025-02-22 11:53:38 +0900

    feat: Change file name

--- src/features/event/components/list/event-item-component.tsx
+++ src/features/event/components/list/event-item-component.tsx
@@ -1,7 +1,7 @@
 import {EventDetail} from "@/features/event/types/event-detail";
 import {ChevronRight, Info} from "lucide-react";
 
-export default function EventItem(props: {event: EventDetail}) {
+export default function EventItemComponent(props: {event: EventDetail}) {
     return (
         <div className={`flex justify-between items-center space-x-2 bg-cyan-400 p-4 rounded-xl shadow-md mb-2`}>
             <Info color="black" size={24}/>
