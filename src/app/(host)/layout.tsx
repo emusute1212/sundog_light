f2c15dc40bdd9e051d559d3ecd59b7b49224464d
emusute1212 <miyanishi1212@gmail.com>
2025-03-08 21:24:32 +0900

    feat: Fix header

--- src/app/(host)/layout.tsx
+++ src/app/(host)/layout.tsx
@@ -1,23 +1,26 @@
-import {signOut} from "@/auth";
+import { auth, signOut } from "@/auth";
 import SundogLightHeader from "@/features/core/components/SundogLightHeader";
 import React from "react";
 
-export default function RootLayout({children}: Readonly<{
-    children: React.ReactNode;
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className={`flex flex-col h-screen items-center`}>
-            <header className={`w-full pb-4 sticky top-0 bg-white`}>
-                <SundogLightHeader
-                    onClickLogoutButton={async () => {
-                        "use server"
-                        await signOut()
-                    }}
-                />
-            </header>
-            <main className={`w-full flex-grow max-w-lg`}>
-                {children}
-            </main>
-        </div>
-    );
+  const session = await auth();
+
+  return (
+    <div className={`flex flex-col h-screen items-center`}>
+      <header className={`w-full pb-4 sticky top-0 bg-white`}>
+        <SundogLightHeader
+          isShowLogoutButton={session != null}
+          onClickLogoutButton={async () => {
+            "use server";
+            await signOut();
+          }}
+        />
+      </header>
+      <main className={`w-full flex-grow max-w-lg`}>{children}</main>
+    </div>
+  );
 }
