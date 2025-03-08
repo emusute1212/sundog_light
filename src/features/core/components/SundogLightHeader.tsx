5100e8474c98f66e40db997b519c11811217f1f0
emusute1212 <miyanishi1212@gmail.com>
2025-03-08 21:24:32 +0900

    feat: Fix header

--- src/features/core/components/SundogLightHeader.tsx
+++ src/features/core/components/SundogLightHeader.tsx
@@ -1,29 +1,36 @@
 import { LogOut } from "lucide-react";
 
-export default function SundogLightHeader({onClickLogoutButton}: {
-    onClickLogoutButton: () => void,
+export default function SundogLightHeader({
+  isShowLogoutButton,
+  onClickLogoutButton,
+}: {
+  isShowLogoutButton: boolean;
+  onClickLogoutButton: () => void;
 }) {
-    return (
-        <div className={`flex items-center justify-between px-4 relative`}>
-            {/* 中央揃えのためのダミー要素 */}
-            <div className="w-20" />
-            
-            {/* タイトル */}
-            <div className="relative">
-                {/* アウトライン付きの背面テキスト */}
-                <h1 className={`
+  return (
+    <div className={`flex items-center justify-between px-4 relative`}>
+      {/* 中央揃えのためのダミー要素 */}
+      <div className="w-20" />
+
+      {/* タイトル */}
+      <div className="relative">
+        {/* アウトライン付きの背面テキスト */}
+        <h1
+          className={`
                     text-4xl 
                     text-center 
                     font-extrabold 
                     p-2
                     text-black
                     [-webkit-text-stroke:2px_black]
-                `}>
-                    SUNDOG Light
-                </h1>
-                
-                {/* グラデーションの前面テキスト */}
-                <h1 className={`
+                `}
+        >
+          SUNDOG Light
+        </h1>
+
+        {/* グラデーションの前面テキスト */}
+        <h1
+          className={`
                     text-4xl 
                     text-center 
                     font-extrabold 
@@ -36,19 +43,24 @@ export default function SundogLightHeader({onClickLogoutButton}: {
                     bg-gradient-to-r
                     from-gray-200
                     to-cyan-200
-                `}>
-                    SUNDOG Light
-                </h1>
-            </div>
-            
-            {/* ログアウトボタン */}
-            <button 
-                onClick={onClickLogoutButton}
-                className="flex items-center justify-center gap-2 hover:text-gray-600 transition-colors"
-            >
-                <LogOut size={18}/>
-                <span>ログアウト</span>
-            </button>
-        </div>
-    );
+                `}
+        >
+          SUNDOG Light
+        </h1>
+      </div>
+
+      {/* ログアウトボタン */}
+      {isShowLogoutButton ? (
+        <button
+          onClick={onClickLogoutButton}
+          className="flex items-center justify-center gap-2 hover:text-gray-600 transition-colors"
+        >
+          <LogOut size={18} />
+          <span>ログアウト</span>
+        </button>
+      ) : (
+        <div className="w-20" />
+      )}
+    </div>
+  );
 }
