c9bf1a6352d9447a8748f9c5faf712fb2139edb1
emusute1212 <miyanishi1212@gmail.com>
2025-03-15 17:16:53 +0900

    fix: Add loading screen

--- src/features/event/components/list/section/EventListSection.tsx
+++ src/features/event/components/list/section/EventListSection.tsx
@@ -1,20 +1,31 @@
-import {EventDetail} from "@/features/event/types/event-detail";
+import { EventDetail } from "@/features/event/types/event-detail";
 import EventItemComponent from "@/features/event/components/list/section/component/EventItemComponent";
 import EventAddComponent from "@/features/event/components/list/section/component/EventAddComponent";
+import EventListSkeleton from "./component/EventListSkeleton";
 
-export default function EventListSection({events}: { events: EventDetail[] }) {
+export default function EventListSection({
+    isLoading,
+    events,
+}: {
+    isLoading: boolean;
+    events: EventDetail[];
+}) {
     return (
         <section className={`grid grid-rows-none px-8`}>
-            {events.map((event: EventDetail) => {
-                    return (
-                        <EventItemComponent
-                            key={event.uuid}
-                            event={event}
-                        />
-                    )
-                }
+            {isLoading ? (
+                <>
+                    {[...Array(3)].map((_, index) => (
+                        <EventListSkeleton key={index} />
+                    ))}
+                </>
+            ) : (
+                <>
+                    {events.map((event: EventDetail) => (
+                        <EventItemComponent key={event.uuid} event={event} />
+                    ))}
+                    <EventAddComponent />
+                </>
             )}
-            <EventAddComponent/>
         </section>
     );
 }
