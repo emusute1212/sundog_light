dd93afd2ce2b07376226e44d297ed3a40e488d7e
emusute1212 <miyanishi1212@gmail.com>
2025-06-01 13:58:50 +0900

    fix: Update LP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -18,16 +19,19 @@ export const metadata: Metadata = {
     description: "Sundog Light app",
 };
 
-export default function RootLayout({children,}: Readonly<{
+export default function RootLayout({
+    children,
+}: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <html lang="ja">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        {children}
-        </body>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <GoogleAnalytics />
+                {children}
+            </body>
         </html>
     );
 }
