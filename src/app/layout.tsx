c0bee125db243551e87decdbc6d5543bc461f1a6
emusute1212 <miyanishi1212@gmail.com>
2025-06-28 23:04:25 +0900

    refactor: change file stracture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import GoogleAnalytics from "./components/GoogleAnalytics";
+import GoogleAnalytics from "@/features/core/components/GoogleAnalytics";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
